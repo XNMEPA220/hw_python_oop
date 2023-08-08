d4058eb8d16961803487a7aa686ad321403d6c86
Егор Рындин <ryndinegr@yandex.ru>
2023-08-08 20:16:32 +0300

    Use dataclass

--- homework.py
+++ homework.py
@@ -1,20 +1,16 @@
 import sys
 from _collections_abc import Sequence as seq
+from dataclasses import dataclass, asdict
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
